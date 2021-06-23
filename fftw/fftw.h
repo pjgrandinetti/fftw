d6fc3a9edf958906d20b61afcd50bbce48b6d367
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2021-06-22 21:29:01 -0400

    initial commit

---
+++ fftw/fftw.h
@@ -0,0 +1,18 @@
+//
+//  fftw.h
+//  fftw
+//
+//  Created by Philip Grandinetti on 3/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fftw.
+FOUNDATION_EXPORT double fftwVersionNumber;
+
+//! Project version string for fftw.
+FOUNDATION_EXPORT const unsigned char fftwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fftw/PublicHeader.h>
+
+
---
+++ fftwf/fftwf.h
@@ -0,0 +1,18 @@
+//
+//  fftwf.h
+//  fftwf
+//
+//  Created by Philip Grandinetti on 3/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fftwf.
+FOUNDATION_EXPORT double fftwfVersionNumber;
+
+//! Project version string for fftwf.
+FOUNDATION_EXPORT const unsigned char fftwfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fftwf/PublicHeader.h>
+
+
