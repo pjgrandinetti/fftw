a881cd7ad9d0bdc8ecd5c9dcc4c511c3522c2a7e
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2022-12-23 11:36:36 -0500

    update project xcode 14.2

--- test/main.c
+++ test/main.c
@@ -9,19 +9,18 @@
 #include "fftw3.h"
 
 int main(int argc, const char * argv[]) {
-    fftwf_plan floatPlan = NULL;
-    fftwf_complex *floatArray = NULL;
     int length = 1024;
     int dft_direction = FFTW_FORWARD;
 
-    if(floatArray == NULL) floatArray = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * length);
-    if(floatPlan == NULL) floatPlan = fftwf_plan_dft_1d((int) length, floatArray, floatArray, dft_direction, FFTW_ESTIMATE);
-    fftwf_execute(floatPlan);
-    if(floatPlan) fftwf_destroy_plan(floatPlan);
+//    fftwf_plan floatPlan = NULL;
+//    fftwf_complex *floatArray = NULL;
+//    if(floatArray == NULL) floatArray = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * length);
+//    if(floatPlan == NULL) floatPlan = fftwf_plan_dft_1d((int) length, floatArray, floatArray, dft_direction, FFTW_ESTIMATE);
+//    fftwf_execute(floatPlan);
+//    if(floatPlan) fftwf_destroy_plan(floatPlan);
 
     fftw_plan plan = NULL;
     fftw_complex *array = NULL;
-
     if(array == NULL) array = (fftw_complex*) fftw_malloc(sizeof(fftwf_complex) * length);
     if(plan == NULL) plan = fftw_plan_dft_1d((int) length, array, array, dft_direction, FFTW_ESTIMATE);
     fftw_execute(plan);
